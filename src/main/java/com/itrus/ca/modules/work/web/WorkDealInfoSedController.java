b9ceb3d09b82049ff1cc0ef2528a69c5f8bd6f15
hu_hao <huhao@robustsoft.cn>
2016-01-09 03:15:17 +0000

    业务中心第二个控制层

---
+++ src/main/java/com/itrus/ca/modules/work/web/WorkDealInfoSedController.java
@@ -0,0 +1,27 @@
+/**
+ * There are <a href="https://github.com/thinkgem/jeesite">JeeSite</a> code generation
+ */
+package com.itrus.ca.modules.work.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itrus.ca.common.web.BaseController;
+
+/**
+ ************************************************************** 
+ ** 
+ ** @author ZhangJingtao
+ ** 
+ **         date:2014年8月12日 上午11:31:54
+ ** 
+ ** @describe:业务办理controller
+ ** 
+ ************************************************************** 
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/work/workDealInfoSed")
+public class WorkDealInfoSedController extends BaseController {
+
+	
+}
