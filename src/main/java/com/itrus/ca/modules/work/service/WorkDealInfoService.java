967c1b88fc3b2b73a42b91cb7462bf0cd5a196f3
ding_jianhui <dingjianhui@robustsoft.cn>
2016-02-19 01:37:17 +0000

    补回业务中心查询方法

--- src/main/java/com/itrus/ca/modules/work/service/WorkDealInfoService.java
+++ src/main/java/com/itrus/ca/modules/work/service/WorkDealInfoService.java
@@ -733,6 +733,68 @@ public class WorkDealInfoService extends BaseService {
 		return workDealInfoDao.find(dc);
 	}
 
+	
+	public Page<WorkDealInfo> find4Apply(Page<WorkDealInfo> page, WorkDealInfo workDealInfo, Date startTime,
+			Date endTime, Long apply) {
+		DetachedCriteria dc = workDealInfoDao.createDetachedCriteria();
+		dc.createAlias("workUser", "workUser");
+		dc.createAlias("workCompany", "workCompany");
+		dc.createAlias("createBy", "createBy");
+		dc.createAlias("createBy.office", "office");
+		dc.createAlias("configApp", "configApp");
+
+		dc.add(Restrictions.or(Restrictions.isNotNull("isIxin"),dataScopeFilterByWorkDealInfo(UserUtils.getUser(), "areaId", "officeId")));
+		
+		//dc.add(dataScopeFilterByWorkDealInfo(UserUtils.getUser(), "areaId", "officeId"));
+		// dc.add(Restrictions.or(Restrictions.eq("dealInfoStatus",
+		// WorkDealInfoStatus.STATUS_CERT_OBTAINED), Restrictions.eq(
+		// "createBy", UserUtils.getUser())));
+
+		if (apply != null) {
+			dc.add(Restrictions.eq("configApp.id", apply));
+		}
+
+		if (startTime != null && endTime != null) {
+			dc.add(Restrictions.ge("notafter", startTime));
+			dc.add(Restrictions.ge("notafter", endTime));
+		}
+		if (workDealInfo.getWorkCompany() != null) {
+			if (workDealInfo.getWorkCompany().getId() != null) {
+				dc.add(Restrictions.eq("workCompany", workDealInfo.getWorkCompany()));
+			}
+			if (workDealInfo.getWorkCompany().getCompanyName() != null
+					&& !workDealInfo.getWorkCompany().getCompanyName().equals("")) {
+				dc.add(Restrictions.like("workCompany.companyName",
+						"%" + EscapeUtil.escapeLike(workDealInfo.getWorkCompany().getCompanyName()) + "%"));
+			}
+			if (workDealInfo.getWorkCompany().getOrganizationNumber() != null
+					&& !workDealInfo.getWorkCompany().getOrganizationNumber().equals("")) {
+				dc.add(Restrictions.like("workCompany.organizationNumber",
+						EscapeUtil.escapeLike(workDealInfo.getWorkCompany().getOrganizationNumber())));
+
+			}
+		}
+		if (workDealInfo.getWorkUser() != null) {
+			if (workDealInfo.getWorkUser().getContactName() != null
+					&& !workDealInfo.getWorkUser().getContactName().equals("")) {
+				dc.add(Restrictions.like("workUser.contactName",
+						"%" + EscapeUtil.escapeLike(workDealInfo.getWorkUser().getContactName()) + "%"));
+			}
+		}
+		if (workDealInfo.getDealInfoStatus() != null && !workDealInfo.getDealInfoStatus().equals("")) {
+			dc.add(Restrictions.eq("dealInfoStatus", workDealInfo.getDealInfoStatus()));
+		}
+		if (StringUtils.isNotEmpty(workDealInfo.getKeySn())) {
+			dc.add(Restrictions.like("keySn", "%" + EscapeUtil.escapeLike(workDealInfo.getKeySn()) + "%"));
+		}
+		if (workDealInfo.getStatus() != null) {
+			dc.add(Restrictions.eq("status", workDealInfo.getStatus()));
+		}
+		dc.add(Restrictions.eq(WorkDealInfo.DEL_FLAG, WorkDealInfo.DEL_FLAG_NORMAL));
+		dc.addOrder(Order.desc("createDate"));
+		return workDealInfoDao.find(page, dc);
+	}
+	
 	public Page<WorkDealInfoListVo> find4Apply(Page<WorkDealInfoListVo> page,
 			WorkDealInfo workDealInfo, Date startTime, Date endTime,
 			Long apply, String productName, Date makeCertStartTime,
