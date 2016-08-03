280f93798a33875f98350f9266890c4c05198272
cao_yichao <caoyichao@robustsoft.cn>
2016-08-03 05:54:55 +0000

    地址js单独放到area.js文件

---
+++ src/main/webapp/static/jquery/area.js
@@ -0,0 +1,97 @@
+//得到省
+	function getPrivince(){
+
+		$.ajax({
+			url : ctx+"/selfArea/findAllPrivince",
+			type : "GET",
+			dataType : "JSON",
+			success : function(d) {
+				
+				var opt = new Option('省份','');
+				$("#s_province").append(opt);
+				$.each(d.list,function(idx, ele) {
+					
+					var var_Option = document.createElement("OPTION");
+					var_Option.text=ele.area_name;
+					var_Option.value=ele.area_name;
+					var_Option.id=ele.area_id;
+					if(ele.area_name == province){
+						
+						var_Option.selected=true;
+					}
+					
+					$("#s_province").append(var_Option);
+	
+				});
+				
+			}
+		});
+	}
+	
+	
+	
+	
+	//得到第二级
+	function getCity(privinceId){
+		$("#s_city").html("");
+		$("#s_county").html("");
+		if(privinceId!= "0"){
+			$.ajax({
+				url : ctx+"/selfArea/findLower?higher="+ privinceId,
+				type : "GET",
+				dataType : "JSON",
+				success : function(d) {
+					var opt = new Option('地级市','');
+					$("#s_city").append(opt);
+					$.each(d.list,function(idx, ele) {
+						var var_Option = document.createElement("OPTION");
+						var_Option.text=ele.area_name;
+						var_Option.value=ele.area_name;
+						var_Option.id=ele.area_id;
+						if(ele.area_name ==city){
+							
+							var_Option.selected=true;
+						}
+						
+						$("#s_city").append(var_Option);
+					});
+					
+				}
+			});
+		}else{
+			$("#s_city").html("<option value = ''>地级市</option>");
+			$("#s_county").html("<option value = ''>市、县级市</option>");
+		}
+	}
+	
+	
+	//得到第三级
+	function getTown(cityId){
+		$("#s_county").html("");
+		if(cityId!= "0"){
+			$.ajax({
+				url : ctx+"/selfArea/findLower?higher="+ cityId,
+				type : "GET",
+				dataType : "JSON",
+				success : function(d) {
+					var opt = new Option('市、县级市','');
+					$("#s_county").append(opt);
+					$.each(d.list,function(idx, ele) {
+						var var_Option = document.createElement("OPTION");
+						var_Option.text=ele.area_name;
+						var_Option.value=ele.area_name;
+						var_Option.id=ele.area_id;
+						if(ele.area_name ==district){
+							
+							var_Option.selected=true;
+						}
+						
+						$("#s_county").append(var_Option);
+					});
+					
+				}
+			});
+		}else{
+			$("#s_county").html("<option value = ''>市、县级市</option>");
+		}
+	}
